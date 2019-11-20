140355710b45b089d0388d58dbabdffc6b82b268
Andrey Kuts <19andreykuts95@gmail.com>
2019-11-20 20:13:55 +0300

    basic chat function

---
+++ src/graphql.js
@@ -0,0 +1,34 @@
+import gql from 'graphql-tag';
+
+export const CHATS_QUERY = gql`
+  query ChatsQuery {
+    chats {
+      id
+      from
+      message
+    }
+  }
+`;
+
+export const SEND_MESSAGE_MUTATION = gql`
+  mutation SendMessage($from: String!, $message: String!) {
+    sendMessage(
+      from: $from,
+      message: $message
+    ) {
+      id
+      from
+      message
+    }
+  }
+`;
+
+export const MESSAGE_SENT_SUBSCRIPTION = gql`
+  subscription MessageSent {
+    messageSent {
+      id
+      from
+      message
+    }
+  }
+`;
